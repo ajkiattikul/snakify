5a4f6bd01ead0d9b2eda39b65b00d28dcdafb5da
jakkaphatminthana <58208814+jakkaphatminthana@users.noreply.github.com>
2019-11-26 19:50:21 +0700

    Create index.php
    หน้าแรก ของเว็บ

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="100%" border="1" cellspacing="0">
+  <tr>
+    <th height="85" colspan="3" scope="col">&nbsp;</th>
+  </tr>
+  <tr>
+    <td width="17%" height="46">&nbsp;</td>
+    <td width="83%" colspan="2" rowspan="11">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="44">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="45">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="44">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="44">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="44">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="43">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="46">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="46">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="47">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="306">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="89" colspan="3">&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
