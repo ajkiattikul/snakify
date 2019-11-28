61aedcbd74410b7e0730710a76fe861df563397a
jakkaphatminthana <58208814+jakkaphatminthana@users.noreply.github.com>
2019-11-28 16:09:04 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -2,9 +2,110 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>MenuPage 1</title>
+<title>MenuPage1</title>
+<style type="text/css">
+#form1 div font a {
+	color: #FFF;
+	font-family: "_Layiji MaHaNiYom V 1.2";
+	font-size: 30px;
+}
+#form2 div table {
+	font-family: "_Layiji MaHaNiYom V 1.2";
+	font-size: 18px;
+}
+body table {
+	font-family: "_Layiji MaHaNiYom V 1.2";
+	font-size: 30px;
+}
+</style>
 </head>
 
 <body>
+<table width="100%" border="1">
+  <tr>
+    <th height="85" colspan="3" scope="col">MenuPage 1</th>
+  </tr>
+  <tr>
+    <td width="17%" height="46" bgcolor="#00CCFF">
+    
+    <form id="form1" name="form1" method="post" action="">
+    	<div align="center">
+        <font size="+2">
+        <a href="page1.php">Menu Page 1</a>
+        </font>
+        </div>   
+    </form></td>
+    
+    <td width="83%" colspan="2" rowspan="11">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="44" bgcolor="#99FFFF">
+    
+    <form id="form2" name="form2" method="post" action="">
+    	<div align="center">
+    	  <table width="117" border="0" cellspacing="0">
+    	    <tr>
+    	      <td width="66"><font size="+2">page 1.1</font></td>
+  	      </tr>
+    	    <tr>
+    	      <td><font size="+2">page 1.2</font></td>
+  	      </tr>
+    	    <tr>
+    	      <td><font size="+2">page 1.3</font></td>
+  	      </tr>
+    	    <tr>
+    	      <td><font size="+2">page 1.4</font></td>
+  	      </tr>
+    	    <tr>
+    	      <td><font size="+2">page 1.5</font></td>
+  	      </tr>
+  	    </table>
+    	</div>
+    </form></td>
+    
+  </tr>
+  <tr>
+    <td height="45">
+    
+    <form id="form2" name="form2" method="post" action="">
+    	<div align="center">
+        <a href="page2.php">Menu Page 2</a>
+        </div>
+    </form></td>
+    
+  </tr>
+  <tr>
+    <td height="44">
+    <form id="form2" name="form2" method="post" action="">
+    	<div align="center">
+        <a href="page3.php">Menu Page 3</a>
+        </div>
+    </form></td>
+  </tr>
+  <tr>
+    <td height="44">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="44">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="43">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="46">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="46">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="47">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="306">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="89" colspan="3">&nbsp;</td>
+  </tr>
+</table>
 </body>
 </html>
