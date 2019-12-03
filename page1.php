06bbe10793f0f9375bb0f095c07a9a7f9d240568
jakkaphatminthana <58208814+jakkaphatminthana@users.noreply.github.com>
2019-12-03 15:38:37 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,111 +1,87 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>MenuPage1</title>
-<style type="text/css">
-#form1 div font a {
-	color: #FFF;
-	font-family: "_Layiji MaHaNiYom V 1.2";
-	font-size: 30px;
-}
-#form2 div table {
-	font-family: "_Layiji MaHaNiYom V 1.2";
-	font-size: 18px;
-}
-body table {
-	font-family: "_Layiji MaHaNiYom V 1.2";
-	font-size: 30px;
-}
-</style>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Snakify</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/simple-sidebar.css" rel="stylesheet" type="text/css">
+  <link href="style.css" rel="stylesheet" type="text/css">
+
 </head>
 
 <body>
-<table width="100%" border="1">
-  <tr>
-    <th height="85" colspan="3" scope="col">MenuPage 1</th>
-  </tr>
-  <tr>
-    <td width="17%" height="46" bgcolor="#00CCFF">
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+      <nav class="navbar navbar-expand-lgborder-bottom" style="background-color:#FF8000">
+        <button class="btn btn-primary" id="menu-toggle" style="background-color:#FFB900"><font color="#FFFFFF">Snakify</font></button>
+        <font color="#FFFFFF">Computer Science</font>
+
+        <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>-->
+      </nav>
+<!---------------------------------------------------------------------------------------------->
+<div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <div class="bg-light border-right border-warning" id="sidebar-wrapper">
     
-    <form id="form1" name="form1" method="post" action="">
-    	<div align="center">
-        <font size="+2">
-        <a href="page1.php">Menu Page 1</a>
-        </font>
-        </div>   
-    </form></td>
-    
-    <td width="83%" colspan="2" rowspan="11">&nbsp;</td>
-  </tr>
-  <tr>
-    <td height="44" bgcolor="#99FFFF">
-    
-    <form id="form2" name="form2" method="post" action="">
-    	<div align="center">
-    	  <table width="117" border="0" cellspacing="0">
-    	    <tr>
-    	      <td width="66"><font size="+2">page 1.1</font></td>
-  	      </tr>
-    	    <tr>
-    	      <td><font size="+2">page 1.2</font></td>
-  	      </tr>
-    	    <tr>
-    	      <td><font size="+2">page 1.3</font></td>
-  	      </tr>
-    	    <tr>
-    	      <td><font size="+2">page 1.4</font></td>
-  	      </tr>
-    	    <tr>
-    	      <td><font size="+2">page 1.5</font></td>
-  	      </tr>
-  	    </table>
-    	</div>
-    </form></td>
-    
-  </tr>
-  <tr>
-    <td height="45">
-    
-    <form id="form2" name="form2" method="post" action="">
-    	<div align="center">
-        <a href="page2.php">Menu Page 2</a>
-        </div>
-    </form></td>
-    
-  </tr>
-  <tr>
-    <td height="44">
-    <form id="form2" name="form2" method="post" action="">
-    	<div align="center">
-        <a href="page3.php">Menu Page 3</a>
-        </div>
-    </form></td>
-  </tr>
-  <tr>
-    <td height="44">&nbsp;</td>
-  </tr>
-  <tr>
-    <td height="44">&nbsp;</td>
-  </tr>
-  <tr>
-    <td height="43">&nbsp;</td>
-  </tr>
-  <tr>
-    <td height="46">&nbsp;</td>
-  </tr>
-  <tr>
-    <td height="46">&nbsp;</td>
-  </tr>
-  <tr>
-    <td height="47">&nbsp;</td>
-  </tr>
-  <tr>
-    <td height="306">&nbsp;</td>
-  </tr>
-  <tr>
-    <td height="89" colspan="3">&nbsp;</td>
-  </tr>
-</table>
+      <div class="list-group list-group-flush">
+        <a href="index.php" class="list-group-item list-group-item-action bg-light">Home</a>
+        
+        <a href="page1.php" class="list-group-item list-group-item-action" style="background:#FFB900"><font color="#FFFFFF">PageMenu1</font></a>
+        <span class="border border-warning">
+        	<ul class="list-group-item bg-light">
+            	<a href="#" class="list-group-item list-group-item-action">SubMenu1</a>
+                <a href="#" class="list-group-item list-group-item-action">SubMenu2</a>
+                <a href="#" class="list-group-item list-group-item-action">SubMenu3</a>
+               
+            </ul>
+             </span>
+            
+            
+        <a href="#" class="list-group-item list-group-item-action bg-light">PageMenu3</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">PageMenu4</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">PageMenu5</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">PageMenu6</a>
+        
+      </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
+<!---------------------------------------------------------------------------------------------->
+      <div class="container-fluid">
+        <h1 class="mt-4">Head</h1>
+        <!--<p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
+        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
+      </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Menu Toggle Script -->
+  <script>
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  </script>
+
 </body>
+
 </html>
